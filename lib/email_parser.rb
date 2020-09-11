fa4982a4293e5e1b31e18e4b364bda288628e26e
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-11 15:08:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,12 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
+  attr_accessor :email_addresses
 
   def initialize
   end
 
   def parse
-  end 
+  end
 
 end
